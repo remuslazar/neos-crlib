7c0f44ae1d7d7522e3e57d23da34ca3ab5961b28
Remus Lazar <rl@cron.eu>
2015-11-10 11:21:00 +0100

    nodetype filter optional, ksort

--- Classes/CRON/CRLib/Command/NodeTypeCommandController.php
+++ Classes/CRON/CRLib/Command/NodeTypeCommandController.php
@@ -9,6 +9,7 @@
 namespace CRON\CRLib\Command;
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TYPO3CR\Domain\Model\NodeType;
 
 /**
  * @Flow\Scope("singleton")
@@ -29,24 +30,34 @@ class NodeTypeCommandController extends \TYPO3\Flow\Cli\CommandController {
 	 * @throws \TYPO3\Flow\Mvc\Exception\StopActionException
 	 * @throws \TYPO3\TYPO3CR\Exception\NodeTypeNotFoundException
 	 */
-	public function showConstraintsCommand($nodeType) {
+	public function showConstraintsCommand($nodeType=null) {
 
-		if (!$this->nodeTypeManager->hasNodeType($nodeType)) {
+		if ($nodeType !== null && !$this->nodeTypeManager->hasNodeType($nodeType)) {
 			$this->outputLine('NodeType: %s not found.', [$nodeType]);
 			$this->quit(1);
 		}
 
-		$nodeType = $this->nodeTypeManager->getNodeType($nodeType);
+		$nodeTypes = $nodeType !== null ? [$this->nodeTypeManager->getNodeType($nodeType)] :
+			$this->nodeTypeManager->getNodeTypes(true);
 
-		$constraints = $nodeType->getConfiguration('constraints.nodeTypes');
-		/** @var \TYPO3\TYPO3CR\Domain\Model\NodeType $super */
-		foreach($nodeType->getDeclaredSuperTypes() as $super) {
-			if ($superConstraints = $super->getConfiguration('constraints.nodeTypes')) {
-				$constraints = array_merge($constraints, $superConstraints);
+		$ret = [];
+		/** @var NodeType $nodeType */
+		foreach ($nodeTypes as $nodeType) {
+			$constraints = $nodeType->getConfiguration('constraints.nodeTypes');
+			/** @var NodeType $super */
+			foreach($nodeType->getDeclaredSuperTypes() as $super) {
+				if ($superConstraints = $super->getConfiguration('constraints.nodeTypes')) {
+					$constraints = array_merge($constraints, $superConstraints);
+				}
+			}
+			if ($constraints) {
+				ksort($constraints);
+				$ret[$nodeType->getName()] = $constraints;
 			}
 		}
 
-		echo json_encode($constraints, JSON_PRETTY_PRINT) . PHP_EOL;
+		ksort($ret);
+		echo json_encode($ret, JSON_PRETTY_PRINT) . PHP_EOL;
 	}
 
 }
