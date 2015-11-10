5d532340c09fbeafadb1d5bde77c0fcb8a4a46cf
Remus Lazar <rl@cron.eu>
2015-11-10 11:48:32 +0100

    --detail implemented

--- Classes/CRON/CRLib/Command/NodeTypeCommandController.php
+++ Classes/CRON/CRLib/Command/NodeTypeCommandController.php
@@ -22,15 +22,31 @@ class NodeTypeCommandController extends \TYPO3\Flow\Cli\CommandController {
 	 */
 	protected $nodeTypeManager;
 
+	private function getConstraints(NodeType $nodeType, $inherited=false) {
+		$constraints = $nodeType->getConfiguration('constraints.nodeTypes');
+		if ($inherited) {
+			/** @var NodeType $super */
+			foreach($nodeType->getDeclaredSuperTypes() as $super) {
+				if ($superConstraints = $super->getConfiguration('constraints.nodeTypes')) {
+					$constraints = array_merge($constraints, $superConstraints);
+				}
+			}
+		}
+		if (is_array($constraints)) ksort($constraints);
+
+		return $constraints;
+	}
+
 	/**
 	 * Show all the Constraints for the specified NodeType
 	 *
 	 * @param string $nodeType
+	 * @param boolean $detail show detailed information about the inheritance
 	 *
 	 * @throws \TYPO3\Flow\Mvc\Exception\StopActionException
 	 * @throws \TYPO3\TYPO3CR\Exception\NodeTypeNotFoundException
 	 */
-	public function showConstraintsCommand($nodeType=null) {
+	public function showConstraintsCommand($nodeType=null, $detail=false) {
 
 		if ($nodeType !== null && !$this->nodeTypeManager->hasNodeType($nodeType)) {
 			$this->outputLine('NodeType: %s not found.', [$nodeType]);
@@ -41,18 +57,19 @@ class NodeTypeCommandController extends \TYPO3\Flow\Cli\CommandController {
 			$this->nodeTypeManager->getNodeTypes(true);
 
 		$ret = [];
+
 		/** @var NodeType $nodeType */
 		foreach ($nodeTypes as $nodeType) {
-			$constraints = $nodeType->getConfiguration('constraints.nodeTypes');
-			/** @var NodeType $super */
-			foreach($nodeType->getDeclaredSuperTypes() as $super) {
-				if ($superConstraints = $super->getConfiguration('constraints.nodeTypes')) {
-					$constraints = array_merge($constraints, $superConstraints);
+			if ($detail) {
+				$ret[$nodeType->getName()] = $this->getConstraints($nodeType, false);
+				foreach($nodeType->getDeclaredSuperTypes() as $super) {
+					if ($superConstraints =  $this->getConstraints($super, false)) {
+						/** @var NodeType $super */
+						$ret['superTypes'][$super->getName()] = $superConstraints;
+					}
 				}
-			}
-			if ($constraints) {
-				ksort($constraints);
-				$ret[$nodeType->getName()] = $constraints;
+			} else {
+				$ret[$nodeType->getName()] = $this->getConstraints($nodeType, true);
 			}
 		}
 
