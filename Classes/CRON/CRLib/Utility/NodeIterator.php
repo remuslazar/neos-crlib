f5cbe9a657f1105d1e66cac6c63447ef25b55bc0
Sebastian Krüger <sk@cron.eu>
2016-08-11 17:12:34 +0200

    Converted to PSR-2 coding style

--- Classes/CRON/CRLib/Utility/NodeIterator.php
+++ Classes/CRON/CRLib/Utility/NodeIterator.php
@@ -19,35 +19,36 @@ use TYPO3\TYPO3CR\Domain\Service\Context;
  * @property \Doctrine\ORM\Internal\Hydration\IterableResult iterator
  * @property bool performClearState
  */
-class NodeIterator implements \Iterator {
+class NodeIterator implements \Iterator
+{
 
-	const BATCH_SIZE = 30;
+    const BATCH_SIZE = 30;
 
-	/**
-	 * @Flow\Inject
-	 * @var \TYPO3\TYPO3CR\Domain\Factory\NodeFactory
-	 */
-	protected $nodeFactory;
+    /**
+     * @Flow\Inject
+     * @var \TYPO3\TYPO3CR\Domain\Factory\NodeFactory
+     */
+    protected $nodeFactory;
 
-	/**
-	 * @Flow\Inject
-	 * @var \TYPO3\TYPO3CR\Domain\Service\ContextFactoryInterface
-	 */
-	protected $contextFactory;
+    /**
+     * @Flow\Inject
+     * @var \TYPO3\TYPO3CR\Domain\Service\ContextFactoryInterface
+     */
+    protected $contextFactory;
 
-	/**
-	 * Inject PersistenceManagerInterface
-	 *
-	 * @Flow\Inject
-	 * @var \TYPO3\Flow\Persistence\PersistenceManagerInterface
-	 */
-	protected $persistenceManager;
+    /**
+     * Inject PersistenceManagerInterface
+     *
+     * @Flow\Inject
+     * @var \TYPO3\Flow\Persistence\PersistenceManagerInterface
+     */
+    protected $persistenceManager;
 
-	/**
-	 * @Flow\Inject
-	 * @var \TYPO3\TYPO3CR\Domain\Repository\NodeDataRepository
-	 */
-	protected $nodeDataRepository;
+    /**
+     * @Flow\Inject
+     * @var \TYPO3\TYPO3CR\Domain\Repository\NodeDataRepository
+     */
+    protected $nodeDataRepository;
 
     /**
      * @var array|null
@@ -55,117 +56,128 @@ class NodeIterator implements \Iterator {
     private $contextOptions;
 
     /**
-	 * @param Query $query The Query object
-	 * @param array $contextOptions Options for the contextFactory::create() method
-	 * @param bool $clearState perform a persistenceManager->clearState() call after BATCH_SIZE
-	 */
-	function __construct(Query $query, $contextOptions=null, $clearState=true) {
-		$this->contextOptions = $contextOptions ? $contextOptions : [
-			// use some viable defaults
-			'invisibleContentShown' => TRUE,
-			'inaccessibleContentShown' => TRUE
-		];
-		$query->useResultCache(false);
-		$query->useQueryCache(false);
-		$this->query = $query;
-		$this->performClearState = $clearState;
-	}
+     * @param Query $query The Query object
+     * @param array $contextOptions Options for the contextFactory::create() method
+     * @param bool $clearState perform a persistenceManager->clearState() call after BATCH_SIZE
+     */
+    function __construct(Query $query, $contextOptions = null, $clearState = true)
+    {
+        $this->contextOptions = $contextOptions ? $contextOptions : [
+            // use some viable defaults
+            'invisibleContentShown' => true,
+            'inaccessibleContentShown' => true
+        ];
+        $query->useResultCache(false);
+        $query->useQueryCache(false);
+        $this->query = $query;
+        $this->performClearState = $clearState;
+    }
 
-	public function initializeObject() {
-		$this->context = $this->contextFactory->create($this->contextOptions);
-		$this->iterator = $this->query->iterate();
-	}
+    public function initializeObject()
+    {
+        $this->context = $this->contextFactory->create($this->contextOptions);
+        $this->iterator = $this->query->iterate();
+    }
 
-	public function clearState() {
-		$this->persistenceManager->persistAll();
-		$this->nodeDataRepository->flushNodeRegistry();
-		/** @var Context $context */
-		foreach ($this->contextFactory->getInstances() as $context) {
-			$context->getFirstLevelNodeCache()->flush();
-		}
-		$this->contextFactory->reset();
-		$this->nodeFactory->reset();
+    public function clearState()
+    {
+        $this->persistenceManager->persistAll();
+        $this->nodeDataRepository->flushNodeRegistry();
+        /** @var Context $context */
+        foreach ($this->contextFactory->getInstances() as $context) {
+            $context->getFirstLevelNodeCache()->flush();
+        }
+        $this->contextFactory->reset();
+        $this->nodeFactory->reset();
 
-		if ($this->performClearState) {
-			$this->persistenceManager->clearState();
-			$this->context = $this->contextFactory->create($this->contextOptions);
-		}
-	}
+        if ($this->performClearState) {
+            $this->persistenceManager->clearState();
+            $this->context = $this->contextFactory->create($this->contextOptions);
+        }
+    }
 
-	/**
-	 * @param NodeData $nodeData
-	 * @return \TYPO3\TYPO3CR\Domain\Model\NodeInterface
-	 * @throws \TYPO3\TYPO3CR\Exception\NodeConfigurationException
-	 */
-	private function getNode(NodeData $nodeData) {
-		$node = $this->nodeFactory->createFromNodeData($nodeData, $this->context);
-		return $node;
-	}
+    /**
+     * @param NodeData $nodeData
+     *
+     * @return \TYPO3\TYPO3CR\Domain\Model\NodeInterface
+     * @throws \TYPO3\TYPO3CR\Exception\NodeConfigurationException
+     */
+    private function getNode(NodeData $nodeData)
+    {
+        $node = $this->nodeFactory->createFromNodeData($nodeData, $this->context);
 
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Return the current element
-	 *
-	 * @link http://php.net/manual/en/iterator.current.php
-	 * @return NodeInterface
-	 */
-	public function current() {
+        return $node;
+    }
 
-		$index = $this->iterator->key();
-		if ($index > 0 && $index % self::BATCH_SIZE === 0) {
-			$this->clearState();
-		}
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     *
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return NodeInterface
+     */
+    public function current()
+    {
 
-		$nodeData = $this->iterator->current();
-		$node = $this->getNode($nodeData[0]);
-		// detach the entity from ORM to save memory
-		//$this->query->getEntityManager()->detach($nodeData[0]);
-		return $node;
-	}
+        $index = $this->iterator->key();
+        if ($index > 0 && $index % self::BATCH_SIZE === 0) {
+            $this->clearState();
+        }
 
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Move forward to next element
-	 *
-	 * @link http://php.net/manual/en/iterator.next.php
-	 * @return void Any returned value is ignored.
-	 */
-	public function next() {
-		$this->iterator->next();
-	}
+        $nodeData = $this->iterator->current();
+        $node = $this->getNode($nodeData[0]);
+        // detach the entity from ORM to save memory
+        //$this->query->getEntityManager()->detach($nodeData[0]);
+        return $node;
+    }
 
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Return the key of the current element
-	 *
-	 * @link http://php.net/manual/en/iterator.key.php
-	 * @return mixed scalar on success, or null on failure.
-	 */
-	public function key() {
-		return $this->iterator->key();
-	}
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     *
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        $this->iterator->next();
+    }
 
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Checks if current position is valid
-	 *
-	 * @link http://php.net/manual/en/iterator.valid.php
-	 * @return boolean The return value will be casted to boolean and then evaluated.
-	 * Returns true on success or false on failure.
-	 */
-	public function valid() {
-		return $this->iterator->valid();
-	}
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     *
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        return $this->iterator->key();
+    }
 
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Rewind the Iterator to the first element
-	 *
-	 * @link http://php.net/manual/en/iterator.rewind.php
-	 * @return void Any returned value is ignored.
-	 */
-	public function rewind() {
-		$this->iterator->rewind();
-	}
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     *
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid()
+    {
+        return $this->iterator->valid();
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     *
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        $this->iterator->rewind();
+    }
 
 }
--- Classes/CRON/CRLib/Utility/NodeQuery.php
+++ Classes/CRON/CRLib/Utility/NodeQuery.php
@@ -7,6 +7,7 @@
  */
 
 namespace CRON\CRLib\Utility;
+
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\QueryBuilder;
 use TYPO3\Flow\Annotations as Flow;
@@ -18,135 +19,154 @@ use TYPO3\Flow\Utility\Unicode\Functions as Unicode;
  * @property null|string initialSearchTermConstraint
  * @property string workspace
  */
-class NodeQuery {
-	/**
-	 * Doctrine's Entity Manager. Note that "ObjectManager" is the name of the related
-	 * interface ...
-	 *
-	 * @Flow\Inject
-	 * @var \Doctrine\Common\Persistence\ObjectManager
-	 */
-	protected $entityManager;
+class NodeQuery
+{
+    /**
+     * Doctrine's Entity Manager. Note that "ObjectManager" is the name of the related
+     * interface ...
+     *
+     * @Flow\Inject
+     * @var \Doctrine\Common\Persistence\ObjectManager
+     */
+    protected $entityManager;
 
-	/**
-	 * Convenience Constructor to initialize the object with some constraints
-	 *
-	 * @param mixed $nodeTypeFilter csv list of NodeTypes to filter
-	 * @param null|string $path filter by path
-	 * @param null|string $searchTerm search term
-	 * @param string $workspace workspace
-	 *
-	 */
-	function __construct($nodeTypeFilter=null, $path=null, $searchTerm=null, $workspace='live') {
-		// we save it as property to apply the constraints later in initializeObject()
-		$this->initialTypeConstraint = $nodeTypeFilter;
-		$this->initialPathConstraint = $path;
-		$this->initialSearchTermConstraint = $searchTerm;
-		$this->workspace = $workspace;
-	}
+    /**
+     * Convenience Constructor to initialize the object with some constraints
+     *
+     * @param mixed $nodeTypeFilter csv list of NodeTypes to filter
+     * @param null|string $path filter by path
+     * @param null|string $searchTerm search term
+     * @param string $workspace workspace
+     *
+     */
+    function __construct($nodeTypeFilter = null, $path = null, $searchTerm = null, $workspace = 'live')
+    {
+        // we save it as property to apply the constraints later in initializeObject()
+        $this->initialTypeConstraint = $nodeTypeFilter;
+        $this->initialPathConstraint = $path;
+        $this->initialSearchTermConstraint = $searchTerm;
+        $this->workspace = $workspace;
+    }
 
-	/** @var QueryBuilder $queryBuilder */
-	public $queryBuilder;
+    /** @var QueryBuilder $queryBuilder */
+    public $queryBuilder;
 
-	public function initializeObject() {
-	    /** @var EntityManager $em */
-	    $em = $this->entityManager;
-		$this->queryBuilder = $em->createQueryBuilder();
-		$this->queryBuilder->select('n')
-			->from('TYPO3\TYPO3CR\Domain\Model\NodeData', 'n')
-		;
+    public function initializeObject()
+    {
+        /** @var EntityManager $em */
+        $em = $this->entityManager;
+        $this->queryBuilder = $em->createQueryBuilder();
+        $this->queryBuilder->select('n')
+                           ->from('TYPO3\TYPO3CR\Domain\Model\NodeData', 'n');
 
-		if (!empty($this->workspace)) {
-			$this->queryBuilder
-				->where('n.workspace IN (:workspaces)')
-				->setParameter('workspaces', $this->workspace)
-			;
-		}
+        if (!empty($this->workspace)) {
+            $this->queryBuilder
+                ->where('n.workspace IN (:workspaces)')
+                ->setParameter('workspaces', $this->workspace);
+        }
 
-		if ($this->initialPathConstraint) $this->addPathConstraint($this->initialPathConstraint);
-		if ($this->initialTypeConstraint) $this->addTypeConstraint($this->initialTypeConstraint);
-		if ($this->initialSearchTermConstraint) $this->addSearchTermConstraint($this->initialSearchTermConstraint);
-		// set default ordering
-		$this->setOrderBy('path', 'ASC');
-	}
+        if ($this->initialPathConstraint) {
+            $this->addPathConstraint($this->initialPathConstraint);
+        }
+        if ($this->initialTypeConstraint) {
+            $this->addTypeConstraint($this->initialTypeConstraint);
+        }
+        if ($this->initialSearchTermConstraint) {
+            $this->addSearchTermConstraint($this->initialSearchTermConstraint);
+        }
+        // set default ordering
+        $this->setOrderBy('path', 'ASC');
+    }
 
-	/**
-	 * @param array|string $types NodeType names
-	 */
-	public function addTypeConstraint($types) {
-		if (is_string($types)) $types = preg_split('/,\s*/', $types);
-		$this->queryBuilder->andWhere('n.nodeType IN (:includeNodeTypes)')
-		                   ->setParameter('includeNodeTypes', $types);
-	}
+    /**
+     * @param array|string $types NodeType names
+     */
+    public function addTypeConstraint($types)
+    {
+        if (is_string($types)) {
+            $types = preg_split('/,\s*/', $types);
+        }
+        $this->queryBuilder->andWhere('n.nodeType IN (:includeNodeTypes)')
+                           ->setParameter('includeNodeTypes', $types);
+    }
 
-	/**
-	 * @param string $path node starting path
-	 */
-	public function addPathConstraint($path) {
-		$this->queryBuilder->andWhere('n.path LIKE :path')
-		                   ->setParameter('path', $path.'%');
-	}
+    /**
+     * @param string $path node starting path
+     */
+    public function addPathConstraint($path)
+    {
+        $this->queryBuilder->andWhere('n.path LIKE :path')
+                           ->setParameter('path', $path . '%');
+    }
 
-	/**
-	 * @param string $term Search Term to search in properties using LIKE
-	 */
-	public function addSearchTermConstraint($term) {
-		// Convert to lowercase, then to json, and then trim quotes from json to have valid JSON escaping.
-		$likeParameter = '%' .
-			str_replace('\\', '\\\\', // escape all \
-				trim(
-					json_encode(Unicode::strtolower($term), JSON_UNESCAPED_UNICODE),
-					'"'
-				)) . '%';
-		$this->queryBuilder->andWhere('n.properties LIKE :term')
-		                   ->setParameter('term', $likeParameter);
-	}
+    /**
+     * @param string $term Search Term to search in properties using LIKE
+     */
+    public function addSearchTermConstraint($term)
+    {
+        // Convert to lowercase, then to json, and then trim quotes from json to have valid JSON escaping.
+        $likeParameter = '%' .
+            str_replace('\\', '\\\\', // escape all \
+                trim(
+                    json_encode(Unicode::strtolower($term), JSON_UNESCAPED_UNICODE),
+                    '"'
+                )) . '%';
+        $this->queryBuilder->andWhere('n.properties LIKE :term')
+                           ->setParameter('term', $likeParameter);
+    }
 
-	/**
-	 * @param string $identifier
-	 */
-	public function addIdentifierConstraint($identifier) {
-		$this->queryBuilder->andWhere('n.identifier LIKE :identifier')
-		                   ->setParameter('identifier', $identifier.'%');
-	}
+    /**
+     * @param string $identifier
+     */
+    public function addIdentifierConstraint($identifier)
+    {
+        $this->queryBuilder->andWhere('n.identifier LIKE :identifier')
+                           ->setParameter('identifier', $identifier . '%');
+    }
 
-	/**
-	 * @param string $sort The ordering expression.
-	 * @param string $order The ordering direction.
-	 */
-	public function setOrderBy($sort, $order) {
-		$this->queryBuilder->orderBy('n.'.$sort, $order);
-	}
+    /**
+     * @param string $sort The ordering expression.
+     * @param string $order The ordering direction.
+     */
+    public function setOrderBy($sort, $order)
+    {
+        $this->queryBuilder->orderBy('n.' . $sort, $order);
+    }
 
-	/**
-	 * Gets the Doctrine ORM Query, results ordered by path ASC
-	 *
-	 * @return \Doctrine\ORM\Query
-	 */
-	public function getQuery() {
-		return $this->queryBuilder->getQuery();
-	}
+    /**
+     * Gets the Doctrine ORM Query, results ordered by path ASC
+     *
+     * @return \Doctrine\ORM\Query
+     */
+    public function getQuery()
+    {
+        return $this->queryBuilder->getQuery();
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getCount() {
-		$this->queryBuilder->select('COUNT(n)');
-		$count = (int)$this->queryBuilder->getQuery()->getSingleScalarResult();
-		$this->queryBuilder->select('n');
-		return $count;
-	}
+    /**
+     * @return int
+     */
+    public function getCount()
+    {
+        $this->queryBuilder->select('COUNT(n)');
+        $count = (int)$this->queryBuilder->getQuery()->getSingleScalarResult();
+        $this->queryBuilder->select('n');
 
-	/**
-	 * Delete all nodes matching the current criteria
-	 *
-	 * @return int number of nodes deleted
-	 */
-	public function deleteAll() {
-		$this->queryBuilder->delete();
-		$count = $this->queryBuilder->getQuery()->execute();
-		$this->queryBuilder->select('n');
-		return $count;
-	}
+        return $count;
+    }
+
+    /**
+     * Delete all nodes matching the current criteria
+     *
+     * @return int number of nodes deleted
+     */
+    public function deleteAll()
+    {
+        $this->queryBuilder->delete();
+        $count = $this->queryBuilder->getQuery()->execute();
+        $this->queryBuilder->select('n');
+
+        return $count;
+    }
 
 }
